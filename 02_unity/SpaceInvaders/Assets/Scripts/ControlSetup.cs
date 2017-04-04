c478603b8f29d55533c428bb9cc37258d2272233
Unknown <jonlopezgarrido@gmail.com>
2017-04-04 22:01:48 +0200

    Ultima version antes de presentar

---
+++ 02_unity/SpaceInvaders/Assets/Scripts/ControlSetup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSetup : MonoBehaviour {
+
+	bool multiPlayer = true;
+
+	// Use this for initialization
+	void Start () {
+
+		if(!this.multiPlayer)
+			Destroy (GameObject.FindWithTag("nave2"));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void BotonSalir(){
+		Application.Quit();
+	}
+
+	public void BotonJugar(){
+		
+		Application.LoadLevel("Nivel1");
+	}
+
+	public void Multijugador (){
+		if (!this.multiPlayer)
+			this.multiPlayer = true;
+		else
+			this.multiPlayer = false;
+	}
+		
+}
---
+++ 02_unity/SpaceInvaders/Assets/Scripts/obj_config.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obj_config : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
